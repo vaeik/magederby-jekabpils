e014d824e276a9e8cc9fedb232947a1867767dc4
Artis <artisv@scandiweb.com>
2020-02-07 20:10:41 +0200

    Initial commit

---
+++ app/design/frontend/Scandiweb/pwa/Magento_Theme/web/precache-manifest.ceb551a0233ae2fff9162c53912b26db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ec45dfc06c55cd8b205",
+    "url": "/static/version1581098184457/frontend/Scandiweb/pwa/en_US/Magento_Theme/main.css"
+  },
+  {
+    "revision": "5ec45dfc06c55cd8b205",
+    "url": "/static/version1581098184457/frontend/Scandiweb/pwa/en_US/Magento_Theme/en_US.bundle.js"
+  },
+  {
+    "revision": "f33ccd8d2b935c123156",
+    "url": "/static/version1581098184457/frontend/Scandiweb/pwa/en_US/Magento_Theme/1.en_US.bundle.js"
+  },
+  {
+    "revision": "482963e0ffe17623fda3",
+    "url": "/static/version1581098184457/frontend/Scandiweb/pwa/en_US/Magento_Theme/2.en_US.bundle.js"
+  },
+  {
+    "revision": "1f912d031a054f062932",
+    "url": "/static/version1581098184457/frontend/Scandiweb/pwa/en_US/Magento_Theme/3.en_US.bundle.js"
+  }
+];
